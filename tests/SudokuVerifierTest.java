bd75a2c2dd5ec1d56768423aa9506d6dfac249a6
Bonny Ham <tram.nguyen@student.oulu.fi>
2022-11-25 15:54:03 +0200

    Add test cases

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -5,12 +5,12 @@ import org.junit.Test;
 public class SudokuVerifierTest {
 
 //implement tests to test Sudokuverifier with boundary values.  Use templates from Task 1 to derive and document test cases.
-	
-// A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
+
+	// A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
-String c = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-String i = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
-SudokuVerifier v = new SudokuVerifier();
+	String c = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	String i = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	SudokuVerifier v = new SudokuVerifier();
 
 	@Test
 	public void testCorrectString() {
@@ -22,8 +22,37 @@ SudokuVerifier v = new SudokuVerifier();
 	public void testIncorrectString() {
 		int a = v.verify(i);
 		assertEquals("incorrect string", a, -2);
-		
+
 	}
-	
-	
+
+	@Test
+	public void testStringContainsNumberZero() {
+		String s = "123456789912345678891234567789123456678912345567891234456789123345678912234567890";
+		int a = v.verify(s);
+		assertEquals("incorrect string", a, -1);
+	}
+
+	@Test
+	public void testStringContainsNegativeNumber_minus1() {
+		String s = "-123456789912345678891234567789123456678912345567891234456789123345678912234567893";
+		int a = v.verify(s);
+		assertEquals("incorrect string", a, -1);
+	}
+
+
+	@Test
+	public void testStringContainsNumberAppearTwice() {
+		String s = "113456789912345678891234567789123456678912345567891234456789123345678912234567893";
+		int a = v.verify(s);
+		assertEquals("incorrect string", a, -2);
+	}
+
+
+	@Test
+	public void testStringContainsNumberBeyondNine_InputNumber10() {
+		String s = "113456789912345678891234567789123456678912345567891234456789123345678912234567810";
+		int a = v.verify(s);
+		assertEquals("incorrect string", a, -1);
+	}
+
 }
